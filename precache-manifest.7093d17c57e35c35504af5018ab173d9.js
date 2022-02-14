441d1ed2f08fd9d35c734b03c9deaf7122c94659
bripap <bpappas302@gmail.com>
2022-02-14 16:01:17 -0500

    Updates

---
+++ precache-manifest.7093d17c57e35c35504af5018ab173d9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e3b6a03b1265b3d5f4bd316a48e703",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "f99448d6b5bbbb89e98c",
+    "url": "/react-portfolio/static/css/main.08325502.chunk.css"
+  },
+  {
+    "revision": "0b6510110444aa4dbb20",
+    "url": "/react-portfolio/static/js/2.aadc951f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio/static/js/2.aadc951f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f99448d6b5bbbb89e98c",
+    "url": "/react-portfolio/static/js/main.ddbe3b3f.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "cc02215da9e2eee4d736ef8243ec15f5",
+    "url": "/react-portfolio/static/media/coding-quiz.cc02215d.png"
+  },
+  {
+    "revision": "402a471b5aad190a33b649f1368f5de1",
+    "url": "/react-portfolio/static/media/cover-image.402a471b.jpg"
+  },
+  {
+    "revision": "a370e1596a1fc061d462351884c01b70",
+    "url": "/react-portfolio/static/media/day-planner.a370e159.png"
+  },
+  {
+    "revision": "5f056ead612a2f857c37acadac97d55f",
+    "url": "/react-portfolio/static/media/food-tracker.5f056ead.png"
+  },
+  {
+    "revision": "c9b5ef529a311d435b98f4632dc442db",
+    "url": "/react-portfolio/static/media/profile-photo.c9b5ef52.jpg"
+  },
+  {
+    "revision": "2fa0cef0f12ede868b4f7e321af84d37",
+    "url": "/react-portfolio/static/media/tech-blog.2fa0cef0.png"
+  },
+  {
+    "revision": "3e482bb64080f7ebd3112efa9a4be114",
+    "url": "/react-portfolio/static/media/tech-takeout.3e482bb6.png"
+  },
+  {
+    "revision": "b647559f4cc50db9fddd03a6a8105874",
+    "url": "/react-portfolio/static/media/weather-dashboard.b647559f.png"
+  }
+]);
