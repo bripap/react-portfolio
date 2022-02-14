b169e0a98ae2fa63f3a193e2433f119f0b2657c0
bripap <bpappas302@gmail.com>
2022-02-14 15:48:30 -0500

    working code

--- Main/src/components/Resume/index.js
+++ Main/src/components/Resume/index.js
@@ -5,7 +5,7 @@ function Resume() {
     <section className="my-5">
       <div className="my-2">
       <p>
-        Download my <a href="https://www.linkedin.com/">resume</a>
+        Download my <a href="https://www.linkedin.com/in/brianna-pappas-66a7b920/overlay/1635482671591/single-media-viewer/" target="_blank">resume</a>
         </p>
         <h3>Front-end Proficiencies</h3>
         <ul className="skills">
